b2c468cb102d3818e1e793c85602df90c188ed51
Alejandro <freyermuthalejandro835@gmail.com>
2023-08-30 14:22:25 +0200

    Arbeit am Login-Client

--- VS_Client/client/src/main/java/client/LoginClient.java
+++ VS_Client/client/src/main/java/client/LoginClient.java
@@ -3,6 +3,7 @@ package client;
 import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.json.JSONObject;
 
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.client.Invocation;
@@ -12,34 +13,73 @@ import jakarta.ws.rs.core.Response;
 
 
 public class LoginClient {
+	private final String SERVER_URL = ""; //TODO
 	private final String LOGIN_URL = ""; //TODO
 	
 	private String username;
 	private String passwd;
 	private JSONObject auth;
 	private Client client;
+	private WebTarget postTarget;
 	
 	public LoginClient(String username, String passwd) {
 		this.username = username;
 		this.passwd = passwd;
+		postTarget = client.target(SERVER_URL).path(LOGIN_URL);
 		
-		auth = new JSONObject();
-		auth.put("Username", this.username);
-		auth.put("Password", this.passwd);
+		auth = new JSONObject().put("Username", this.username).put("Password", this.passwd);
 		
 		client = JerseyClientBuilder.newClient();
 	}
 	
-	public Response login() {
-		WebTarget target = client.target(LOGIN_URL);
-		Invocation.Builder invocation = target.request(MediaType.APPLICATION_JSON);
-		
-		
-		//Response res = client.target(URL).request().post(Entity.xml(auth));
+	
+	/**
+	 * Sendet Benutzername und Passwort als JSON-Daten per POST-Request an den Server.
+	 * @return Gibt ein Response Objekt zurück, das die Antwort des Servers enthält
+	 */
+	private Response postLoginCredentials() {
+		Invocation.Builder invocation = postTarget.request(MediaType.APPLICATION_JSON);
+		Response res = invocation.post(Entity.entity(auth, MediaType.APPLICATION_JSON));
+		System.out.println(String.format("Status: %i \n Response %s \n", res.getStatus(), res.readEntity(String.class)));
 		return res;
 	}
 	
-	//TODO: Response des Servers auswerten
+	/**
+	 * 
+	 * @param res Response Objekt das die Serverantwort enthält
+	 * @return true falls der Server das Paar <Benutzername, Passwort> in seiner DB gefunden hat, sonst false
+	 */
+	private boolean loginErfolgreich(Response res) {
+		//TODO
+	}
+	
+	/**
+	 * Wertet den boolean isAdmin in der Serverantwort aus.
+	 * @param res Response Objekt das die Serverantwort enthält
+	 * @return true falls isAdmin = true, sonst false
+	 */
+	private boolean isAdmin(Response res) {
+		//TODO
+	}
+	
+	
+	private boolean serverStatusOk(Response res) {
+		boolean isStatusOk = true;
+		int serverStatus = res.getStatus();
+		if(serverStatus != /*ServerStatus*/) {
+			isStatusOk = false;
+		}
+		
+		return isStatusOk;
+	}
+	
+	public void login() {
+		Response serverResponse = postLoginCredentials();
+		//TODO: Onlinestatus
+		 //TODO: Status testen
+	}
+	
+	
 	
 	
 	
