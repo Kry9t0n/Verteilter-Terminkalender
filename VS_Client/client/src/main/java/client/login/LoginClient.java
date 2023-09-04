50ad6324bfac29cc3aac1d59674d996a26556689
Alejandro <freyermuthalejandro835@gmail.com>
2023-09-04 18:52:42 +0200

    Update LoginClient.java

--- VS_Client/client/src/main/java/client/login/LoginClient.java
+++ VS_Client/client/src/main/java/client/login/LoginClient.java
@@ -13,88 +13,74 @@ import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-
 public class LoginClient {
-	private final String TARGET_URL = "http:/localhost:8080/resttest/webapi/users"; //TODO: echte URL einfügen!!!
-	private final int SERVER_STATUS_OK = 200;
-	
+	private final String TARGET_URL = "http:/localhost:8080/resttest/webapi/users"; // TODO: echte URL einfügen!!!
+
 	private Benutzer loginBenutzer;
 	private ObjectNode auth;
 	private Client client;
-	
+
 	public LoginClient(String username, String passwd) {
 		this.loginBenutzer = new Benutzer(passwd, username);
-		
+
 		client = ClientBuilder.newClient();
 	}
-	
-	
+
 	/**
-	 * Sendet Benutzername und Passwort als JSON-Daten per POST-Request an den Server.
+	 * Sendet Benutzername und Passwort als JSON-Daten per POST-Request an den
+	 * Server.
+	 * 
 	 * @return Gibt ein Response Objekt zurück, das die Antwort des Servers enthält
 	 */
 	public Response postLoginCredentials() {
-		return client
-				.target(TARGET_URL)
-				.request(MediaType.APPLICATION_JSON)
+		return client.target(TARGET_URL).request(MediaType.APPLICATION_JSON)
 				.post(Entity.entity(loginBenutzer, MediaType.APPLICATION_JSON));
 	}
-	
+
 	/**
 	 * 
 	 * @param res Response Objekt das die Serverantwort enthält
-	 * @return true falls der Server das Paar <Benutzername, Passwort> in seiner DB gefunden hat, sonst false
+	 * @return true falls der Server das Paar <Benutzername, Passwort> in seiner DB
+	 *         gefunden hat, sonst false
 	 */
-	private boolean loginErfolgreich(Response res) {
-		//TODO
-		return false; //Einfach ein Wert damit Eclipse nicht mehr meckert
+	private boolean loginErfolgreich(JsonNode node) { // alle Attribute gleich -1 => login fehlgeschlagen
+		boolean loginErfolgreich = false;
+
+		if (node.get("benutzerId").asInt() == -1 && node.get("benutzerName").asText().equals("-1")
+				&& node.get("passwort").asText().equals("-1") && node.get("name").asText().equals("-1")
+				&& node.get("vorname").asText().equals("-1") && node.get("isAdmin").asInt() == -1) {
+			return loginErfolgreich; // login fehlgeschlagen => loginErfolgreich = false
+		} else {
+			loginErfolgreich = true;
+		}
+		return loginErfolgreich;
+
 	}
-	
+
 	/**
 	 * Wertet den boolean isAdmin in der Serverantwort aus.
+	 * 
 	 * @param res Response Objekt das die Serverantwort enthält
 	 * @return true falls isAdmin = true, sonst false
 	 */
-	public boolean isAdmin(Response res) {
-		String responseBody = res.readEntity(String.class);
-		ObjectMapper mapper = new ObjectMapper();
-		
-		JsonNode node = null;
-		try {
-			node = mapper.readTree(responseBody);
-		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return node.get("isAdmin").asBoolean();
+	private boolean isAdmin(JsonNode node) {
+		// ist isAdmin in der Antwort des Servers auf 1 gesetzt bedeutet das, dass der
+		// user admin rechte hat
+		return node.get("isAdmin").asInt() == 1;
 	}
-	
-	
-	private boolean serverStatusOk(Response res) {
-		boolean isStatusOk = true;
-		int serverStatus = res.getStatus();
-		if(serverStatus != SERVER_STATUS_OK) { //Server-Code 200 = alles ok!!!
-			isStatusOk = false;
-		}
-		
-		return isStatusOk;
-	}
-	
-	public void login() {
+
+	public void login() throws JsonMappingException, JsonProcessingException, Exception {
 		Response serverResponse = postLoginCredentials();
-		//TODO: Onlinestatus
-		 //TODO: Status testen
+		// TODO: Onlinestatus
+		JsonNode node = new ObjectMapper().readTree(serverResponse.readEntity(String.class));
+		if(!loginErfolgreich(node)) {
+			throw new Exception("Login war nicht erfolgreich! Kombination aus Username und Passwort stimmt nicht überein!");
+		}
 	}
-	
-	
-	
+
 	public static void main(String[] args) {
 		LoginClient l = new LoginClient("Alejandro", "k12d64fai45lf");
 		l.postLoginCredentials();
 	}
-	
-	
+
 }
