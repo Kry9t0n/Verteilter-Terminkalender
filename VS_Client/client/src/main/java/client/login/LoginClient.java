68e7d5bc6eaf8b81d4e3e1408c1d4641b4f77252
Alejandro <freyermuthalejandro835@gmail.com>
2023-09-05 10:29:20 +0200

    LoginClient abgeschlossen
    LoginClient ist nun lauffähig und funktioniert. Zusätzlich wurden kleine Änderungen an der Benutzerklasse vorgenommen.

--- VS_Client/client/src/main/java/client/login/LoginClient.java
+++ VS_Client/client/src/main/java/client/login/LoginClient.java
@@ -14,14 +14,16 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 public class LoginClient {
-	private final String TARGET_URL = "http:/localhost:8080/resttest/webapi/users"; // TODO: echte URL einfügen!!!
+	private final String TARGET_URL = "http://localhost:8080/resttest/webapi/benutzer"; // TODO: echte URL einfügen!!!
 
 	private Benutzer loginBenutzer;
 	private ObjectNode auth;
 	private Client client;
 
-	public LoginClient(String username, String passwd) {
-		this.loginBenutzer = new Benutzer(passwd, username);
+	public LoginClient(String username, String passwd, Benutzer masterUser) {
+		this.loginBenutzer = masterUser;
+		this.loginBenutzer.setBenutzerName(username);
+		this.loginBenutzer.setPasswort(passwd);
 
 		client = ClientBuilder.newClient();
 	}
@@ -32,7 +34,7 @@ public class LoginClient {
 	 * 
 	 * @return Gibt ein Response Objekt zurück, das die Antwort des Servers enthält
 	 */
-	public Response postLoginCredentials() {
+	private Response postLoginCredentials() {
 		return client.target(TARGET_URL).request(MediaType.APPLICATION_JSON)
 				.post(Entity.entity(loginBenutzer, MediaType.APPLICATION_JSON));
 	}
@@ -43,12 +45,11 @@ public class LoginClient {
 	 * @return true falls der Server das Paar <Benutzername, Passwort> in seiner DB
 	 *         gefunden hat, sonst false
 	 */
-	private boolean loginErfolgreich(JsonNode node) { // alle Attribute gleich -1 => login fehlgeschlagen
+	private boolean loginErfolgreich(Benutzer user) { // alle Attribute gleich -1 => login fehlgeschlagen
 		boolean loginErfolgreich = false;
 
-		if (node.get("benutzerId").asInt() == -1 && node.get("benutzerName").asText().equals("-1")
-				&& node.get("passwort").asText().equals("-1") && node.get("name").asText().equals("-1")
-				&& node.get("vorname").asText().equals("-1") && node.get("isAdmin").asInt() == -1) {
+		if (user.getBenutzerId() == -1 && user.getBenutzerName().equals("-1") && user.getPasswort().equals("-1")
+				&& user.getName().equals("-1") && user.getVorname().equals("-1") && user.getIsAdmin() == -1) {
 			return loginErfolgreich; // login fehlgeschlagen => loginErfolgreich = false
 		} else {
 			loginErfolgreich = true;
@@ -57,30 +58,44 @@ public class LoginClient {
 
 	}
 
-	/**
-	 * Wertet den boolean isAdmin in der Serverantwort aus.
-	 * 
-	 * @param res Response Objekt das die Serverantwort enthält
-	 * @return true falls isAdmin = true, sonst false
-	 */
-	private boolean isAdmin(JsonNode node) {
-		// ist isAdmin in der Antwort des Servers auf 1 gesetzt bedeutet das, dass der
-		// user admin rechte hat
-		return node.get("isAdmin").asInt() == 1;
-	}
-
 	public void login() throws JsonMappingException, JsonProcessingException, Exception {
 		Response serverResponse = postLoginCredentials();
 		// TODO: Onlinestatus
-		JsonNode node = new ObjectMapper().readTree(serverResponse.readEntity(String.class));
-		if(!loginErfolgreich(node)) {
-			throw new Exception("Login war nicht erfolgreich! Kombination aus Username und Passwort stimmt nicht überein!");
+		Benutzer benutzerObjectFromServerResponse = new ObjectMapper()
+				.readValue(serverResponse.readEntity(String.class), Benutzer.class);
+		// JsonNode node = new
+		// ObjectMapper().readTree(serverResponse.readEntity(String.class));
+		if (!loginErfolgreich(benutzerObjectFromServerResponse)) {
+			throw new Exception(
+					"Login war nicht erfolgreich! Kombination aus Username und Passwort stimmt nicht überein!");
+		} else {
+			this.loginBenutzer.setBenutzerId(benutzerObjectFromServerResponse.getBenutzerId());
+			this.loginBenutzer.setName(benutzerObjectFromServerResponse.getName());
+			this.loginBenutzer.setVorname(benutzerObjectFromServerResponse.getVorname());
+			this.loginBenutzer.setIsAdmin(benutzerObjectFromServerResponse.getIsAdmin());
 		}
 	}
-
+	
+	//Nur zum Testen!!!!
+	/*
 	public static void main(String[] args) {
-		LoginClient l = new LoginClient("Alejandro", "k12d64fai45lf");
-		l.postLoginCredentials();
-	}
+
+		Benutzer masterUser = new Benutzer();
+		
+		System.out.println("MasterUser zuerst: " + masterUser);
+		
+		LoginClient l = new LoginClient("Alejandro", "k12d64fai45lf", masterUser);
+		try {
+			l.login();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println("MasterUser danach: " + masterUser);
+	}*/
 
 }
