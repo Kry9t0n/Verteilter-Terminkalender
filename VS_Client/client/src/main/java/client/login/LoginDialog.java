d7895becd7bc5bb26a4704db138bc332a6e04cd5
Alejandro <freyermuthalejandro835@gmail.com>
2023-09-05 13:48:03 +0200

    Arbeit am LoginDialog

--- VS_Client/client/src/main/java/client/login/LoginDialog.java
+++ VS_Client/client/src/main/java/client/login/LoginDialog.java
@@ -1,5 +1,57 @@
 package client.login;
 
-public class LoginDialog {
+import java.util.Scanner;
 
+import client.Benutzer;
+import client.mastercontroller.MasterController;
+
+public class LoginDialog {
+	private Benutzer masterUser;
+	private LoginClient login;
+	private Scanner scanner;
+	
+	public LoginDialog(Benutzer masterUser) {
+		this.masterUser = masterUser;
+		scanner = new Scanner(System.in);
+	}
+	
+	private void start() {
+		dialogAusgeben();
+		int eingabe = eingabeLesen();
+		LoginDialogOptions option = LoginDialogOptions.getOptionByOrdinal(eingabe);
+		optionAusfuehren(option);
+	}
+	
+	private void dialogAusgeben() {
+		System.out.println("### LoginDialog ###");
+		for(LoginDialogOptions op : LoginDialogOptions.values()) {
+			System.out.println(op.getInfo() + ": " + op.ordinal());
+		}
+	}
+	
+	private int eingabeLesen() {
+		System.out.print("Bitte Optionsauswahl eingeben: ");
+		return scanner.nextInt();
+	}
+	
+	private void optionAusfuehren(LoginDialogOptions option) {
+		switch (option) {
+		case LOGIN:
+			fuehreLoginAus();
+			break;
+		case CANCEL:
+			cancel();
+			break;
+		}
+	}
+	
+	private void fuehreLoginAus() {
+		
+	}
+	
+	
+	private void cancel() {
+		MasterController.programmBeenden();
+	}
+	
 }
---
+++ VS_Client/client/src/main/java/client/mastercontroller/MasterController.java
@@ -0,0 +1,14 @@
+package client.mastercontroller;
+
+import client.Benutzer;
+
+public class MasterController {
+	private Benutzer masterUser;
+	
+	
+	public static void programmBeenden() {
+		
+	}
+	
+	
+}
