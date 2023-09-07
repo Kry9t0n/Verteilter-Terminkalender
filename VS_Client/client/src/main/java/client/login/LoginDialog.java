b7d4641e3c541375a3ca44ab5047ac658ebd3c54
Alejandro <freyermuthalejandro835@gmail.com>
2023-09-07 21:19:41 +0200

    MasterController und LoginDialog
    LoginDialog ist soweit fertig, muss nur noch ein Bug gefixt werden.
    MasterController: rudimentäre Outline

--- VS_Client/client/src/main/java/client/login/LoginDialog.java
+++ VS_Client/client/src/main/java/client/login/LoginDialog.java
@@ -1,6 +1,10 @@
 package client.login;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 import client.Benutzer;
 import client.mastercontroller.MasterController;
@@ -9,32 +13,72 @@ public class LoginDialog {
 	private Benutzer masterUser;
 	private LoginClient login;
 	private Scanner scanner;
-	
+
 	public LoginDialog(Benutzer masterUser) {
 		this.masterUser = masterUser;
 		scanner = new Scanner(System.in);
+		start();
 	}
-	
+
 	private void start() {
-		dialogAusgeben();
-		int eingabe = eingabeLesen();
-		LoginDialogOptions option = LoginDialogOptions.getOptionByOrdinal(eingabe);
-		optionAusfuehren(option);
+		boolean loginSuccess;
+
+		do {
+			loginSuccess = true;
+			try {
+				dialogAusgeben();
+				int eingabe = eingabeLesen();
+				LoginDialogOptions option = LoginDialogOptions.getOptionByOrdinal(eingabe);
+				optionAusfuehren(option);
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+				loginSuccess = false;
+			} catch (InputMismatchException e) {
+				System.out.println("Fehler! Achtung: NUR Zahlen eingeben!");
+				loginSuccess = false;
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				loginSuccess = false;
+			}
+		} while (!loginSuccess);
+
 	}
-	
+
 	private void dialogAusgeben() {
 		System.out.println("### LoginDialog ###");
-		for(LoginDialogOptions op : LoginDialogOptions.values()) {
+		System.out.println("Verfügbare Optionen: ");
+		for (LoginDialogOptions op : LoginDialogOptions.values()) {
 			System.out.println(op.getInfo() + ": " + op.ordinal());
 		}
 	}
-	
+
 	private int eingabeLesen() {
-		System.out.print("Bitte Optionsauswahl eingeben: ");
-		return scanner.nextInt();
+		int choice = 0;
+		boolean input_accepted = true;
+
+		do {
+			System.out.println("Bitte Optionsauswahl eingeben: ");
+			if (!scanner.hasNextInt()) {
+
+				input_accepted = false;
+				System.out.println("Es werden nur Zahlen akzeptiert. Eingabe bitte wiederholen!");
+
+				throw new InputMismatchException();
+
+			} else {
+				choice = scanner.nextInt();
+			}
+		} while (!input_accepted);
+
+		return choice;
 	}
-	
-	private void optionAusfuehren(LoginDialogOptions option) {
+
+	private void optionAusfuehren(LoginDialogOptions option)
+			throws JsonMappingException, JsonProcessingException, Exception {
 		switch (option) {
 		case LOGIN:
 			fuehreLoginAus();
@@ -44,14 +88,32 @@ public class LoginDialog {
 			break;
 		}
 	}
-	
-	private void fuehreLoginAus() {
-		
+
+	private void fuehreLoginAus() throws JsonMappingException, JsonProcessingException, Exception {
+
+		// Werte von Benutzer einlesen
+		System.out.println("### Login: ###");
+		System.out.print("Bitte Username eingeben: ");
+		String username = scanner.next();
+		System.out.print("Bitte Passwort eingeben: ");
+		String password = scanner.next();
+
+		// LoginClient starten
+		LoginClient l_client = new LoginClient(username, password, masterUser);
+		l_client.login();
 	}
-	
-	
+
 	private void cancel() {
 		MasterController.programmBeenden();
 	}
 	
+	//Nur zum Testen!!!
+	/*
+	public static void main(String[] args) {
+		Benutzer master = new Benutzer();
+		LoginDialog ld = new LoginDialog(master);
+
+		System.out.println(master);
+	}*/
+
 }
--- VS_Client/client/src/main/java/client/mastercontroller/MasterController.java
+++ VS_Client/client/src/main/java/client/mastercontroller/MasterController.java
@@ -1,14 +1,50 @@
 package client.mastercontroller;
 
 import client.Benutzer;
+import client.login.LoginDialog;
 
 public class MasterController {
 	private Benutzer masterUser;
 	
+	public MasterController() {
+		masterUser = new Benutzer();
+		run();
+	}
 	
-	public static void programmBeenden() {
+	private void run() {
+		fuehreLoginClientAus();
+		selectAndRunClient();
+	}
+	
+	private void fuehreLoginClientAus() {
+		LoginDialog ld = new LoginDialog(masterUser);
+	}
+	
+	private void selectAndRunClient() {
+		if(masterUser.getIsAdmin() == 1) { // führe Admin Client aus
+			fuehreAdminClientAus();
+		}else if(masterUser.getIsAdmin() == 0) { // führe normalen Benutzerclient aus
+			fuehreNormalenClientAus();
+		}else {
+			System.err.println("Fehler! Programm wird beendet...");
+			return;
+		}
+	}
+	
+	private void fuehreNormalenClientAus() {
 		
 	}
 	
+	private void fuehreAdminClientAus() {
+		
+	}
 	
+	public static void programmBeenden() {
+		System.out.println("Programm wird beendet...");
+		return;
+	}
+	
+	public static void main(String[] args) {
+		new MasterController();
+	}
 }
