4d80ecf90bb9c9297cc35af8ef76a4f7a78b581c
xXRaz3erXx <ygeber@aplusg.de>
2023-09-12 16:38:34 +0200

    OnlineStatus Intervall
    Intervall und extra Klasse für die Abfrage des Online Status abgelegt (10min)

---
+++ VS_Client/client/src/main/java/client/login/StatusIntervall.java
@@ -0,0 +1,17 @@
+package client.login;
+
+
+import java.util.TimerTask;
+
+public class StatusIntervall extends TimerTask {
+
+    @Override
+    public void run() {
+        //Methode schreiben welche alle 10 Minuten ausgeführt wird
+        //Request an Server (Tabelle wo online Clients gespreichert)
+        //Antwort auswerten 
+    }
+
+
+    
+}
