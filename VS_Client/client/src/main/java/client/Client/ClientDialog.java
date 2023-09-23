ee378cc12d2bc59cd6b18ecdf22bc439980ca84f
Alejandro Freyermuth <51532709+Kry9t0n@users.noreply.github.com>
2023-09-23 17:28:15 +0200

    Add files via upload

--- VS_Client/client/src/main/java/client/Client/ClientDialog.java
+++ VS_Client/client/src/main/java/client/Client/ClientDialog.java
@@ -1,6 +1,8 @@
 package client.Client;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -9,6 +11,9 @@ import client.TerminRessoucen;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 
+/*
+ * @author Alexei Brandt
+ */
 public class ClientDialog {
 	private Scanner input;
 	private Client client;
@@ -19,6 +24,7 @@ public class ClientDialog {
 	private static final int TERMINE_AUSGEBEN = 2;
 	private static final int EINLADUNGEN_AUSGEBEN = 3;
 	private static final int Termin_Bearbeiten = 4;
+	private static final int TERMIN_MIT_BESTIMMTEN_DATUM = 5;
 	
 	public ClientDialog(BenutzerClient benutzerClient) {
 		this.benutzerClient = benutzerClient;
@@ -52,7 +58,6 @@ public class ClientDialog {
 
 	private void eingabeAusfuehren(int eingabe) {
 		
-		//Integer.parseInt(String.valueOf(eingabe));
 		
 		switch(eingabe){
 		case 1: 
@@ -67,14 +72,44 @@ public class ClientDialog {
 		case 4:
 			terminbearbeiten();
 			break;
+		case 5:
+			termin_mit_bestimmten_datum();
+			break;
 		case 0:
 			System.out.println("ClientDialog wurde beendet!");
 			break;
 		default:
 			System.out.println("Eingabe falsch!");
-			//throw new InputMismatchException("Keine Strings nur Integer!");
 		}
 		
+	}
+
+	private void termin_mit_bestimmten_datum() {
+		System.out.println("Geben Sie bitte das Datum an, um die Termine zuerhalten\n");
+		
+		int jahr;
+		System.out.println("Jahr:");
+		jahr = leseGanzzahlEingabe();
+		
+		int monat;
+		System.out.println("Monat:");
+		monat = leseGanzzahlEingabe();
+		
+		int tag;
+		System.out.println("tag:");
+		tag = leseGanzzahlEingabe();
+		
+		LocalDate date = LocalDate.of(jahr,monat,tag); 
+		
+		ArrayList<Termin> tagesListe = benutzerClient.fetchAlleTermineEinesTages(date);
+		
+		String ausgabe = "Termine am:"+ jahr + monat + tag + "\n";
+		for (Termin termin: tagesListe) {
+			ausgabe = ausgabe + termin.toString() + "\n";
+		}
+		
+		System.out.println(ausgabe.toString());
+	
 	}
 
 	private void terminbearbeiten() {
@@ -92,9 +127,39 @@ public class ClientDialog {
 		//termin wird mittels id geholt
 		//termin toString
 		Termin updateTermin = TerminRessoucen.getEinzelTerminByID(client, terminid); //TODO: prüfen ob das so passt
-		System.out.println("Welcher Parameter soll veraendert werden?");
+		
+		//System.out.println("Welcher Parameter soll veraendert werden?");
+		//System.out.println("------------------------------------------\n");
+		System.out.println("Geben Sie die neuen Parameter an?");
 		System.out.println("------------------------------------------\n");
 		
+		System.out.println("Neuer Titel:");
+		titel = input.next();
+		updateTermin.setTitel(titel);
+		
+		System.out.println("Neues Jahr:");
+		jahr = leseGanzzahlEingabe();
+		
+		System.out.println("Neuer Monat:");
+		monat = leseGanzzahlEingabe();
+		
+		System.out.println("Neuer Tag:");
+		tag = leseGanzzahlEingabe();
+		
+		System.out.println("Neue Stunde:");
+		stunde = leseGanzzahlEingabe();
+		
+		System.out.println("Neue Minuten:");
+		minute = leseGanzzahlEingabe();
+		
+		LocalDateTime date = LocalDateTime.of(jahr,monat,tag,stunde,minute); 
+		String datum = date.toString();
+		updateTermin.setDatum(date);
+		
+		System.out.println("Neue Dauer:");
+		dauer = leseGanzzahlEingabe();
+		updateTermin.setDauer(dauer);
+		/*
 		int eingabe;
 		do {
 			System.out.print("1:Titel\n"
@@ -112,6 +177,7 @@ public class ClientDialog {
 			case 1: 
 				System.out.println("Neuer Titel:");
 				titel = input.next();
+				updateTermin.setTitel(titel);
 				break;
 			case 2:
 				System.out.println("Neues Jahr:");
@@ -136,13 +202,14 @@ public class ClientDialog {
 			case 7:
 				System.out.println("Neue Dauer:");
 				dauer = leseGanzzahlEingabe();
+				updateTermin.setDauer(dauer);
 				break;
 			case 0:
 				break;
 			default:
 				System.out.println("Eingabe falsch!");
 		}
-	} while(eingabe != 0);
+	} while(eingabe != 0);*/
 		//TerminRessoucen terminressource;
 		TerminRessoucen.updateTermin(client, updateTermin);
 		System.out.println("Termin wuerde bearbeitet!");
@@ -157,9 +224,21 @@ public class ClientDialog {
 	}
 
 	private void termineAusgeben() {
+		benutzerClient.fetchTermineInDarstellungszeitraum();
+		
+		ArrayList<ArrayList<Termin>> alleTermine = benutzerClient.getTerminSpeicher();
+		
 		System.out.println("------------------------------------------\n");
 		System.out.println("Ausgabe der Termine!");
 		System.out.println("------------------------------------------\n");
+		
+		for (ArrayList<Termin> terminListe : alleTermine) {
+		    for (Termin termin : terminListe) {
+		        System.out.println(termin.toString());
+		    }
+			System.out.println("------------------------------------------\n");
+		}
+	
 	}
 		
 	
@@ -167,6 +246,7 @@ public class ClientDialog {
 	private void terminErstellen() {
 		String einladungBenutzer;
 		String eingabe2 = "";
+		
 		System.out.println("Titel:");
 		String titel = input.next();
 		
@@ -205,11 +285,15 @@ public class ClientDialog {
 		else {
 			einladungBenutzer = "";	
 		}
-		Termin termin = new Termin(titel,date,dauer,10,einladungBenutzer);
+		
+		Benutzer benutzer;
+		benutzer = benutzerClient.getBenutzer();
+		
+		Termin termin = new Termin(titel,date,dauer,benutzer.getBenutzerId(),einladungBenutzer);
 		TerminRessoucen.addTermin(client, termin);
 		
 		System.out.println("Termin wuerde erstellt !!!");
-		System.out.println(termin);
+		//System.out.println(termin);
 		
 	}
 
@@ -228,6 +312,7 @@ public class ClientDialog {
 				+ TERMINE_AUSGEBEN + ":Termin ausgeben\n"
 				+ EINLADUNGEN_AUSGEBEN + ":Einladungen ausgeben\n"
 				+ Termin_Bearbeiten + ":Termin bearbeiten\n"
+				+ TERMIN_MIT_BESTIMMTEN_DATUM + ":Termin mit bestimmten Datum ausgeben\n"
 				+ ENDE + ":Ende\n");
 	}
 	
