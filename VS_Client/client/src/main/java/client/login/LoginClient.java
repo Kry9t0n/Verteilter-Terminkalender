a86cc27076619ea7f9592fc7c39f4c9ba58b10a7
Alejandro <freyermuthalejandro835@gmail.com>
2023-09-23 20:10:44 +0200

    Update LoginClient.java

--- VS_Client/client/src/main/java/client/login/LoginClient.java
+++ VS_Client/client/src/main/java/client/login/LoginClient.java
@@ -87,9 +87,7 @@ public class LoginClient {
 	private boolean loginErfolgreich(Benutzer user) { // alle Attribute gleich -1 => login fehlgeschlagen
 		boolean loginErfolgreich = false;
 		
-		// alte Bedingung: user.getBenutzerId() == -1 && user.getBenutzerName().equals("-1") && user.getPasswort().equals("-1")
-		// && user.getName().equals("-1") && user.getVorname().equals("-1") && user.getIsAdmin() == -1
-		if (user.getBenutzerId() == 0 && user.getIsAdmin() == 0) {
+		if (user.getBenutzerId() == -1 && user.getBenutzerName().equals("-1") && user.getPasswort().equals("-1") && user.getName().equals("-1") && user.getVorname().equals("-1") && user.getIsAdmin() == -1) {
 			return loginErfolgreich; // login fehlgeschlagen => loginErfolgreich = false
 		} else {
 			loginErfolgreich = true;
