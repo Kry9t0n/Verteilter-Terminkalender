a810a158e675f3c61268ed3eaae69755be4d20d4
Alejandro <freyermuthalejandro835@gmail.com>
2023-09-26 22:15:08 +0200

    Update AdminClient.java
    Funktion um Monitoringdaten vom Server zu fetchen.
    ABER parsingError bei Umwandlung von JSON zu Monitoring_Data Objekt.

--- VS_Client/client/src/main/java/client/Admin/AdminClient.java
+++ VS_Client/client/src/main/java/client/Admin/AdminClient.java
@@ -1,8 +1,17 @@
 package client.Admin;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import client.Benutzer;
+import client.Monitoring_Data;
+import client.ServerResourceBaseURL;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 public class AdminClient {
 
@@ -20,6 +29,16 @@ public class AdminClient {
 
 	public Benutzer getBenutzer() {
 		return benutzer;
+	}
+	
+	public Monitoring_Data fetcheServerStatistiken() throws JsonMappingException, JsonProcessingException {
+		Response res = client
+						.target(ServerResourceBaseURL.EINGELADEN_BASE_URL.getURL())
+						.request(MediaType.APPLICATION_JSON)
+						.get();
+		
+		Monitoring_Data statData = new ObjectMapper().readValue(res.readEntity(String.class), Monitoring_Data.class);
+		return statData;
 	}
 
 
